1d08fa6fec91880c9e8f529f4495271b862fab08
techdozor <srazin@msn.com>
2023-03-20 23:58:30 +0000

    latest

--- ffmpeg_audio_truncate.py
+++ ffmpeg_audio_truncate.py
@@ -1,5 +1,3 @@
-#This Python script is licensed under the MIT License.
-
 #Copyright (c) 2023 Semantic Science
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
@@ -8,31 +6,21 @@
 
 #THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import whisper
-import csv
 import os
-import datetime
+import subprocess
 
-# Define the directory path where .wav files are located
-directory_path = '.' #set directory location with audio files
 
-model = whisper.load_model('medium', 'cpu')
+#wav file input
+input_dir = './'
+output_dir = './testout'
 
-csv_filename = 'outputresult.csv'
 
-fieldnames = ['date','filename','transcription']
+#loop through wav files
 
-with open(csv_filename, 'w', newline='') as file:
-    writer = csv.DictWriter(file, fieldnames=fieldnames)
-    writer.writeheader()
-
-# Iterate over the files in the directory
-for filename in os.listdir(directory_path):
-    if filename.endswith('.wav'): # check if the file is a .wav file
-        result = model.transcribe(filename)
-        print(filename, result["text"])
-        data = {'date': datetime.datetime.now(), 'filename': filename, 'transcription': result["text"]}
-
-        with open(csv_filename, 'a', newline='') as file:
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writerow(data)
+for filename in os.listdir(input_dir):
+    if filename.endswith(".WAV"):
+        #create input and output files
+        input_path = os.path.join(input_dir, filename)
+        #output_path = os.path.join(output_dir, "truncated_" + filename)
+        output_path = "truncated_" + filename
+        subprocess.run(["ffmpeg", "-i", input_path, "-t", "120", output_path])
